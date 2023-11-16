8f2f62928f91a24556d637c930a09618a9b256d6
luch1994 <1097650398@qq.com>
2023-11-16 14:24:49 +0800

    fix: 修改默认模板，修改投票题数据回显问题

--- server/src/apps/surveyPublish/service/surveyKeyStoreService.ts
+++ server/src/apps/surveyPublish/service/surveyKeyStoreService.ts
@@ -2,7 +2,7 @@ import { mongo } from '../db/mongo'
 
 // 该服务用于模拟redis
 class SurveyKeyStoreService {
-    getKeyStoreResult(surveyKeyStoreData:Array<any>) {
+    getKeyStoreResult(surveyKeyStoreData: Array<any>) {
         const surveyKeyStoreReult = {}
         for (const surveyKeyStoreItem of surveyKeyStoreData) {
             surveyKeyStoreReult[surveyKeyStoreItem.key] = surveyKeyStoreItem.data
@@ -10,29 +10,29 @@ class SurveyKeyStoreService {
         return surveyKeyStoreReult
     }
 
-    async set({surveyPath, key, data, type}) {
-        const surveyKeyStore = await mongo.getCollection({collectionName:'surveyKeyStore'});
+    async set({ surveyPath, key, data, type }) {
+        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
         const setResult = await surveyKeyStore.updateOne({
             key,
             surveyPath,
             type
-        },{
-            $set:{
+        }, {
+            $set: {
                 key,
                 surveyPath,
                 type,
                 data,
-                createDate:Date.now(),
-                updateDate:Date.now(),
+                createDate: Date.now(),
+                updateDate: Date.now(),
             }
-        },{
-            upsert:true //如果不存在则插入
+        }, {
+            upsert: true //如果不存在则插入
         })
         return setResult
     }
 
-    async get({surveyPath,key,type}) {
-        const surveyKeyStore = await mongo.getCollection({collectionName:'surveyKeyStore'});
+    async get({ surveyPath, key, type }) {
+        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
         const surveyKeyStoreData = await surveyKeyStore.findOne({
             key,
             surveyPath,
@@ -41,16 +41,16 @@ class SurveyKeyStoreService {
         return surveyKeyStoreData?.data
     }
 
-    async getAll({surveyPath,keyList,type}) {
-        const surveyKeyStore = await mongo.getCollection({collectionName:'surveyKeyStore'});
+    async getAll({ surveyPath, keyList, type }) {
+        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
         const surveyKeyStoreData = await surveyKeyStore.find({
-            key:{$in:keyList},
+            key: { $in: keyList },
             surveyPath,
             type
         }).toArray()
         return this.getKeyStoreResult(surveyKeyStoreData)
     }
-    
+
 }
 
 export const surveyKeyStoreService = new SurveyKeyStoreService()
--- server/src/apps/surveyPublish/service/surveyPublishService.ts
+++ server/src/apps/surveyPublish/service/surveyPublishService.ts
@@ -1,18 +1,18 @@
 import { mongo } from '../db/mongo'
 import { surveyKeyStoreService } from './surveyKeyStoreService'
-import {CommonError} from '../../../types/index'
+import { CommonError } from '../../../types/index'
 
 class SurveyPublishService {
-    async get({surveyPath}:{surveyPath:string}) { 
-        const surveyMeta = await mongo.getCollection({collectionName:'surveyMeta'});
-        const surveyMetaData = await surveyMeta.findOne({surveyPath})
-        if(!surveyMetaData) {
+    async get({ surveyPath }: { surveyPath: string }) {
+        const surveyMeta = await mongo.getCollection({ collectionName: 'surveyMeta' });
+        const surveyMetaData = await surveyMeta.findOne({ surveyPath })
+        if (!surveyMetaData) {
             throw new CommonError('该问卷已不存在')
         }
         const surveyMetaRes = mongo.convertId2StringByDoc(surveyMetaData)
-        const surveyPublish = await mongo.getCollection({collectionName:'surveyPublish'});
-        const surveyPublishData = await surveyPublish.findOne({pageId:surveyMetaRes._id.toString()},{sort:{createDate:-1}})
-        if(!surveyPublishData) {
+        const surveyPublish = await mongo.getCollection({ collectionName: 'surveyPublish' });
+        const surveyPublishData = await surveyPublish.findOne({ pageId: surveyMetaRes._id.toString() }, { sort: { createDate: -1 } })
+        if (!surveyPublishData) {
             throw new CommonError('该问卷未发布')
         }
         const surveyPublishRes = mongo.convertId2StringByDoc(surveyPublishData)
@@ -22,10 +22,10 @@ class SurveyPublishService {
         }
     }
 
-    async queryVote({surveyPath,voteKeyList}:{surveyPath:string, voteKeyList:Array<string>}) {
-        return await surveyKeyStoreService.getAll({surveyPath,keyList:voteKeyList,type:'vote'})
+    async queryVote({ surveyPath, voteKeyList }: { surveyPath: string, voteKeyList: Array<string> }) {
+        return await surveyKeyStoreService.getAll({ surveyPath, keyList: voteKeyList, type: 'vote' })
     }
-    
+
 }
 
 export const surveyPublishService = new SurveyPublishService()

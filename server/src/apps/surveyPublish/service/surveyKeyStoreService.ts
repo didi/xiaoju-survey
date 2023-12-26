1c2bb83916f854209a59e62816fa5e7f72c1ce7b
luch1994 <1097650398@qq.com>
2023-12-26 16:09:13 +0800

    feat: eslint完成一部分

--- server/src/apps/surveyPublish/service/surveyKeyStoreService.ts
+++ server/src/apps/surveyPublish/service/surveyKeyStoreService.ts
@@ -1,56 +1,56 @@
-import { mongo } from '../db/mongo'
+import { mongo } from '../db/mongo';
 
 // 该服务用于模拟redis
 class SurveyKeyStoreService {
-    getKeyStoreResult(surveyKeyStoreData: Array<any>) {
-        const surveyKeyStoreReult = {}
-        for (const surveyKeyStoreItem of surveyKeyStoreData) {
-            surveyKeyStoreReult[surveyKeyStoreItem.key] = surveyKeyStoreItem.data
-        }
-        return surveyKeyStoreReult
+  getKeyStoreResult(surveyKeyStoreData: Array<any>) {
+    const surveyKeyStoreReult = {};
+    for (const surveyKeyStoreItem of surveyKeyStoreData) {
+      surveyKeyStoreReult[surveyKeyStoreItem.key] = surveyKeyStoreItem.data;
     }
+    return surveyKeyStoreReult;
+  }
 
-    async set({ surveyPath, key, data, type }) {
-        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
-        const setResult = await surveyKeyStore.updateOne({
-            key,
-            surveyPath,
-            type
-        }, {
-            $set: {
-                key,
-                surveyPath,
-                type,
-                data,
-                createDate: Date.now(),
-                updateDate: Date.now(),
-            }
-        }, {
-            upsert: true //如果不存在则插入
-        })
-        return setResult
-    }
+  async set({ surveyPath, key, data, type }) {
+    const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
+    const setResult = await surveyKeyStore.updateOne({
+      key,
+      surveyPath,
+      type
+    }, {
+      $set: {
+        key,
+        surveyPath,
+        type,
+        data,
+        createDate: Date.now(),
+        updateDate: Date.now(),
+      }
+    }, {
+      upsert: true //如果不存在则插入
+    });
+    return setResult;
+  }
 
-    async get({ surveyPath, key, type }) {
-        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
-        const surveyKeyStoreData = await surveyKeyStore.findOne({
-            key,
-            surveyPath,
-            type
-        })
-        return surveyKeyStoreData?.data
-    }
+  async get({ surveyPath, key, type }) {
+    const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
+    const surveyKeyStoreData = await surveyKeyStore.findOne({
+      key,
+      surveyPath,
+      type
+    });
+    return surveyKeyStoreData?.data;
+  }
 
-    async getAll({ surveyPath, keyList, type }) {
-        const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
-        const surveyKeyStoreData = await surveyKeyStore.find({
-            key: { $in: keyList },
-            surveyPath,
-            type
-        }).toArray()
-        return this.getKeyStoreResult(surveyKeyStoreData)
-    }
+  async getAll({ surveyPath, keyList, type }) {
+    const surveyKeyStore = await mongo.getCollection({ collectionName: 'surveyKeyStore' });
+    const surveyKeyStoreData = await surveyKeyStore.find({
+      key: { $in: keyList },
+      surveyPath,
+      type
+    }).toArray();
+    return this.getKeyStoreResult(surveyKeyStoreData);
+  }
 
 }
 
-export const surveyKeyStoreService = new SurveyKeyStoreService()
+export const surveyKeyStoreService = new SurveyKeyStoreService();
--- server/src/rpc.ts
+++ server/src/rpc.ts
@@ -1,11 +1,12 @@
-export async function rpcInvote<P, R>(appServerName: string, params: P): Promise<R> {
-    const appServerNameData = /^(\w+)\.(\w+)$/.exec(appServerName);
-    if (!appServerNameData) {
-        throw new Error('rpc调用必须按照app.function名方式填写，app和function名称只支持数字字母下划线')
-    }
-    const appName = appServerNameData[1];
-    const serverName = appServerNameData[2];
-    const App = require(`./apps/${appName}/index`).default
-    const instance = new App()
-    return await instance[serverName](params)
+import appRegistry from './registry';
+
+export function rpcInvote<P, R>(appServerName: string, params: P): Promise<R> {
+  const appServerNameData = /^(\w+)\.(\w+)$/.exec(appServerName);
+  if (!appServerNameData) {
+    throw new Error('rpc调用必须按照app.function名方式填写，app和function名称只支持数字字母下划线');
+  }
+  const appName = appServerNameData[1];
+  const serverName = appServerNameData[2];
+  const instance = appRegistry.getApp(appName.toLowerCase());
+  return instance[serverName](params);
 }

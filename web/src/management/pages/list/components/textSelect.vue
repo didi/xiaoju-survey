40da65cc46098f5cb0796e0cfff7977ac375ee7d
xiaoyaoabiha_v <xiaoyaoabiha_v@didiglobal.com>
2024-01-18 10:29:48 +0800

    feat: 问卷列表新增类型和状态筛选

---
+++ web/src/management/pages/list/components/textSelect.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="text-select-root">
+        <el-select v-model="selectValue" :placeholder="options.label">
+        <el-option
+          v-for="item in options.value"
+          :key="item.value"
+          :label="item.label"
+          :value="item.value">
+        </el-option>
+      </el-select>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TextSelect',
+        data(){
+            return {
+                selectValue: this.options.default
+            }
+        },
+        props: {
+            options: {
+                type: Object,
+                required: true
+            },
+            effectFun:{
+                type: Function
+            },
+            effectKey: {
+                type: String
+            }
+        },
+        watch: {
+            selectValue(newSelect){
+                const {effectFun} = this
+                typeof effectFun === 'function' && effectFun(newSelect, this.effectKey)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .el-select{
+      width: 120px;
+      margin-right: 20px;
+      ::v-deep .el-input__inner{
+        border: none;
+      }
+   
+    }
+</style>

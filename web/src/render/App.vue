49d87617cd63c3d2590fd2d784550fca303853d4
dayou <853094838@qq.com>
2024-05-15 14:48:04 +0800

    perl: 删除多余代码

--- web/src/render/App.vue
+++ web/src/render/App.vue
@@ -17,9 +17,7 @@ import AlertDialog from './components/AlertDialog.vue'
 
 import LogoIcon from './components/LogoIcon.vue'
 import { get as _get } from 'lodash-es'
-import { ruleConf } from '@/common/logicEngine/ruleConf'
-import { init } from '@/render/hooks/useRuleEngine.js'
-init(ruleConf)
+import { initRuleEngine } from '@/render/hooks/useRuleEngine.js'
 
 export default {
   name: 'App',
@@ -71,8 +69,7 @@ export default {
             this.setSkin(skinConf)
             this.$store.commit('setSurveyPath', surveyPath)
             this.$store.dispatch('init', questionData)
-            // this.$store.dispatch('initRuleEngine', logicConf?.showLogicConf);
-            init(logicConf?.showLogicConf)
+            initRuleEngine(logicConf?.showLogicConf)
             this.$store.dispatch('getEncryptInfo')
           } else {
             throw new Error(res.errmsg)

f9cc64cf5f85bedab5e9bf0afd644302b856f4f1
alwayrun <minge23@live.cn>
2024-05-24 13:05:51 +0800

    refactor: 重构 management/pages/publishResult 目录下组件, 使用 Vue3 组合式 API 写法

--- web/src/management/pages/publish/components/ChannelRow.vue
+++ web/src/management/pages/publish/components/ChannelRow.vue
@@ -4,63 +4,66 @@
       <div class="left">
         <div class="crc-url-wrap" :class="{ 'no-name': !data.name }">
           <div style="margin-bottom: 16px">
-            <el-input class="cru-content" :model-value="getFullUrl(data)" :readonly="true" />
+            <el-input class="cru-content" :model-value="normalizationURL(data)" :readonly="true" />
           </div>
         </div>
       </div>
       <div class="operate-btn-group">
         <el-tooltip class="item" effect="dark" content="在新页面打开" placement="top">
-          <a class="cru-suffix j-open" @click="openPage(getFullUrl(data))">
+          <a class="cru-suffix j-open" @click="handleOpenPage(normalizationURL(data))">
             <i class="font23 iconfont icon-jinru"></i>
           </a>
         </el-tooltip>
         <el-tooltip class="item" effect="dark" content="复制链接" placement="top">
-          <a class="cru-suffix j-copy" :data-clipboard-text="getFullUrl(data)" @click="handleCopy">
+          <a
+            class="cru-suffix j-copy"
+            :data-clipboard-text="normalizationURL(data)"
+            @click="handleCopy"
+          >
             <i class="font23 iconfont icon-fuzhi"></i>
           </a>
         </el-tooltip>
-        <QRCode class="cru-suffix" :url="getFullUrl(data)" />
+        <QRCode class="cru-suffix" :url="normalizationURL(data)" />
       </div>
     </div>
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import Clipboard from 'clipboard'
-
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/src/message.scss'
 
 import QRCode from './QRCode.vue'
 
-export default {
-  name: 'ChannelRow',
-  components: {
-    QRCode
-  },
-  props: ['data', 'styleWrap'],
-  methods: {
-    handleCopy() {
-      const clipboard = new Clipboard('.j-copy')
-      clipboard.on('success', (e) => {
-        ElMessage({
-          type: 'success',
-          message: `已复制渠道链接：${e.text}`
-        })
-      })
-    },
-    openPage(url) {
-      window.open(url)
-    },
-    getFullUrl(v) {
-      const url = v.fullUrl
-      const protocol = window.location.protocol
-      return `${protocol}//${url.split('//')[1]}`
-    }
-  }
+interface Props {
+  data: any
+  styleWrap: any
+}
+
+defineProps<Props>()
+
+const handleCopy = () => {
+  const clipboard = new Clipboard('.j-copy')
+  clipboard.on('success', (e) => {
+    ElMessage({
+      type: 'success',
+      message: `已复制渠道链接：${e.text}`
+    })
+
+    clipboard.destroy()
+  })
+}
+
+const handleOpenPage = (url: string) => window.open(url)
+
+const normalizationURL = (value: any) => {
+  const url = value.fullUrl
+  const protocol = window.location.protocol
+
+  return `${protocol}//${url.split('//')[1]}`
 }
 </script>
-
 <style lang="scss" scoped>
 .channel-list-wrap div.normal-row-wrap {
   position: relative;

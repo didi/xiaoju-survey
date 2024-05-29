4115ff9847a9e779484a14b3ea6da6f2444f0fe3
chaorenluo <1243357953@qq.com>
2024-05-29 21:59:13 +0800

    refactor:统一B,C端渲染组件,将其抽离到物料区 (#184)
    * feat:抽离B，C端通用组件
    
    * refactor: 通用组件统一渲染
    
    * 兼容修复问题 (+1 squashed commit)
    Squashed commits:
    [8d168ef] refactor: 替换统一渲染组件
    
    * refactor:统一B,C端渲染组件,将其抽离到物料区

---
+++ web/src/materials/communals/widgets/HeaderContent/index.jsx
@@ -0,0 +1,47 @@
+import { defineComponent } from 'vue'
+import './index.scss'
+import HeaderBanner from './Components/HeaderBanner'
+import HeaderVideo from './Components/HeaderVideo'
+
+export default defineComponent({
+  name: 'HeaderContent',
+  props: {
+    bannerConf: {
+      type: Object,
+      default: () => {}
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    isSelected: {
+      type: Boolean,
+      default: false
+    }
+  },
+  emits: ['select'],
+  setup(props, { emit }) {
+    const handleClick = () => {
+      if (props.readonly) return
+      emit('select')
+    }
+
+    return {
+      handleClick,
+      props
+    }
+  },
+  render() {
+    const { bannerConf, readonly, isSelected } = this.props
+    return (
+      <div class="header-content-warp" onClick={this.handleClick}>
+        <HeaderBanner bannerConf={bannerConf} readonly={readonly} isSelected={isSelected} />
+        {bannerConf?.bannerConfig?.videoLink ? (
+          <HeaderVideo bannerConf={bannerConf} readonly={readonly} />
+        ) : (
+          ''
+        )}
+      </div>
+    )
+  }
+})

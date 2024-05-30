f31cd0f77311667f51008e45a4c21992f48b9730
xixiIBN5100 <144224541+xixiIBN5100@users.noreply.github.com>
2024-05-30 14:40:53 +0800

    perf: 问卷编辑页标题优化 (#240)
    * refactor: 重构 src/management/pages/edit/modules 目录下组件, 使用 Vue3 组合式 API 写法
    
    * perf: 问卷编辑页标题优化

--- web/src/management/pages/edit/modules/generalModule/TitlePanel.vue
+++ web/src/management/pages/edit/modules/generalModule/TitlePanel.vue
@@ -1,26 +1,77 @@
 <template>
-  <div class="title">
-    {{ title }}
+  <div class="title-container">
+    <div
+        class="title"
+        @mouseover="showFullTitle"
+        @mousemove="updateTooltipPosition"
+        @mouseleave="hideFullTitle"
+    >
+      {{ title }}
+    </div>
+    <div
+        class="tooltip"
+        v-if="tooltipVisible"
+        :style="{ top: tooltipPosition.top + 'px', left: tooltipPosition.left + 'px' }"
+    >
+      {{ title }}
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'TitlePanel',
-  props: {
-    title: {
-      type: String,
-      default: ''
-    }
+<script setup lang="ts">
+import { ref } from 'vue'
+import { defineProps } from 'vue'
+
+defineProps({
+  title: {
+    type: String,
+    default: ''
   }
+})
+
+const tooltipVisible = ref(false)
+const tooltipPosition = ref({ top: 0, left: 0 })
+
+const showFullTitle = () => {
+  tooltipVisible.value = true
+}
+
+const updateTooltipPosition = (event: MouseEvent) => {
+  tooltipPosition.value = {
+    top: event.clientY + 10,
+    left: event.clientX + 10
+  }
+}
+
+const hideFullTitle = () => {
+  tooltipVisible.value = false
 }
 </script>
 
 <style lang="scss" scoped>
+.title-container {
+  position: relative;
+  width: 280px;
+}
+
 .title {
   overflow: hidden;
   text-overflow: ellipsis;
-  width: 400px;
+  white-space: nowrap;
+  width: 100%;
+  cursor: pointer;
+}
+
+.tooltip {
+  position: fixed;
+  z-index: 10;
+  background: white;
+  padding: 5px;
+  border: 1px solid #ddd;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  max-width: 1200px;
   white-space: nowrap;
 }
 </style>
+
+

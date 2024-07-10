32e43b8260fad3d709af50339bcbb234a1952f0e
Ken <66313154+HeHasGun@users.noreply.github.com>
2024-07-10 18:06:39 +0800

    feat: 协作者管理优化和皮肤标签样式调整 (#333)
    * feat: 皮肤设置内主题分类标签样式调整
    
    * feat: 协作者管理优化

---
+++ web/src/management/pages/edit/components/CooperationPanel.vue
@@ -0,0 +1,32 @@
+<template>
+  <slot name="content" :onCooper="onCooper"></slot>
+  <CooperModify :modifyId="cooperId" :visible="cooperModify" @on-close-codify="onCooperClose" />
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import 'element-plus/theme-chalk/src/message.scss'
+import CooperModify from '@/management/pages/list/components/CooperModify.vue';
+import { useRoute } from 'vue-router';
+
+defineSlots<{
+  content: (scope: { onCooper: () => void }) => any
+}>();
+
+const route = useRoute()
+const surveyId = route.params?.id as string;
+const cooperModify = ref<boolean>(false);
+const cooperId = ref<string>('');
+
+const onCooper = (): void => {
+  cooperId.value = surveyId;
+  cooperModify.value = true;
+};
+
+const onCooperClose = (): void => {
+  cooperModify.value = false;
+};
+
+</script>
+<style lang="scss" scoped>
+
+</style>

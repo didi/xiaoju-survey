f7467edaab91213552867585340df860e08cf0b3
jiangchunfu <jiangchunfu@kaike.la>
2024-08-19 16:03:35 +0800

    build: add optimizeDeps packages

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -67,7 +67,9 @@ export default defineConfig({
       'yup',
       'crypto-js/sha256',
       'element-plus/es/locale/lang/zh-cn',
-      'node-forge'
+      'node-forge',
+      '@logicflow/core',
+      '@logicflow/extension'
     ]
   },
   plugins: [

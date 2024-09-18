3158c54c8c4138a0127992a118bcbd9628f76ef0
dayou <853094838@qq.com>
2024-09-18 15:31:18 +0800

    fix: lint

--- web/src/render/stores/question.js
+++ web/src/render/stores/question.js
@@ -5,7 +5,7 @@ import { useSurveyStore } from '@/render/stores/survey'
 import { queryVote } from '@/render/api/survey'
 import { QUESTION_TYPE, NORMAL_CHOICES } from '@/common/typeEnum'
 import { parseJson } from '@/render/utils/index'
-import { VOTE_INFO_KEY, QUOTA_INFO_KEY } from '@/render/utils/constant' 
+import { VOTE_INFO_KEY } from '@/render/utils/constant' 
 
 
 // 投票进度逻辑聚合

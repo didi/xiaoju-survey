4b2e84967130839603ffc66248328ba6274cff74
liweitang <levitang@126.com>
2025-05-19 23:18:33 +0800

    [Feature]:文件服务：OSS对接七牛云oss完成

---
+++ survey-dal/src/main/java/com/xiaojusurvey/engine/oss/qiniuyun/QiniuYunService.java
@@ -0,0 +1,84 @@
+package com.xiaojusurvey.engine.oss.qiniuyun;
+
+import com.qiniu.http.Response;
+import com.qiniu.storage.DownloadUrl;
+import com.qiniu.storage.UploadManager;
+import com.qiniu.util.Auth;
+import com.xiaojusurvey.engine.common.constants.RespErrorCode;
+import com.xiaojusurvey.engine.common.exception.FileException;
+import com.xiaojusurvey.engine.common.util.FileUtil;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.io.InputStream;
+
+@Component
+@ConditionalOnBean({QiniuYunConfig.class})
+public class QiniuYunService {
+
+    @Resource
+    private QiniuYunProperties qiniuYunProperties;
+
+    @Resource
+    private Auth auth;
+
+    @Resource
+    private UploadManager uploadManager;
+
+    /**
+     * 上传文件
+     *
+     * @param bucket      桶名
+     * @param filename    文件名
+     * @param inputStream 文件流
+     * @return 文件上传后的响应
+     */
+    public Response putObject(String bucket, @NotBlank String filename, @NotNull InputStream inputStream) {
+        if (!StringUtils.hasText(bucket)) {
+            bucket = qiniuYunProperties.getBucket();
+        }
+        try {
+            String token = auth.uploadToken(bucket);
+            return uploadManager.put(inputStream, filename, token, null, null);
+        } catch (Exception e) {
+            throw new FileException("QiniuYun OSS 发生错误：" + e.getMessage(), RespErrorCode.OSS_CLIENT_ERROR.getCode());
+        }
+    }
+
+    /**
+     * 获取文件访问链接
+     *
+     * @param filename    文件名
+     * @param expiredTime 过期时间（秒）
+     * @return 文件访问链接
+     */
+    public String getObjectUrl(String filename, String expiredTime) {
+        if (!qiniuYunProperties.getNeedPrivateRead()) {
+            return (qiniuYunProperties.getUseSSL() ? "https" : "http") + "://" + qiniuYunProperties.getEndpoint() + "/" + filename;
+        }
+        if (!StringUtils.hasText(expiredTime)) {
+            expiredTime = qiniuYunProperties.getExpiredTime();
+        }
+        try {
+            DownloadUrl url = new DownloadUrl(qiniuYunProperties.getEndpoint(), qiniuYunProperties.getUseSSL(), filename);
+            long deadline = System.currentTimeMillis() / 1000 + FileUtil.parseExpiryTimeToSeconds(expiredTime);
+            return url.buildURL(auth, deadline);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取文件前缀路径
+     *
+     * @return 文件前缀路径
+     */
+    public String getFilePath() {
+        return qiniuYunProperties.getPathPrefix();
+    }
+
+}
---
+++ survey-extensions/src/main/java/com/xiaojusurvey/engine/extensions/file/handler/impl/QiniuYunFileHandler.java
@@ -0,0 +1,57 @@
+package com.xiaojusurvey.engine.extensions.file.handler.impl;
+
+import com.qiniu.http.Response;
+import com.xiaojusurvey.engine.common.constants.RespErrorCode;
+import com.xiaojusurvey.engine.common.enums.FileProviderEnum;
+import com.xiaojusurvey.engine.common.exception.FileException;
+import com.xiaojusurvey.engine.common.util.FileUtil;
+import com.xiaojusurvey.engine.extensions.file.handler.FileHandler;
+import com.xiaojusurvey.engine.extensions.file.model.FileReq;
+import com.xiaojusurvey.engine.extensions.file.model.FileResp;
+import com.xiaojusurvey.engine.oss.qiniuyun.QiniuYunService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.InputStream;
+import java.util.Objects;
+
+@Component
+@Slf4j
+public class QiniuYunFileHandler extends FileHandler {
+
+    @Resource
+    private QiniuYunService qiniuYunService;
+
+    @Override
+    public FileProviderEnum getProvider() {
+        return FileProviderEnum.QINIU_OSS;
+    }
+
+    @Override
+    public FileResp uploadFile(FileReq fileReq) {
+        MultipartFile file = fileReq.getFile();
+        if (Objects.isNull(file) || file.isEmpty()) {
+            throw new FileException("上传文件为空", RespErrorCode.UPLOAD_FILE_ERROR.getCode());
+        }
+        String filename = FileUtil.generateUniqueFilename(file.getOriginalFilename());
+        String filePath = qiniuYunService.getFilePath();
+        String key = FileUtil.join(filePath, filename);
+        try (InputStream inputStream = file.getInputStream()) {
+            Response response = qiniuYunService.putObject(null, key, inputStream);
+            if (Objects.isNull(response) || !response.isOK()) {
+                throw new FileException("上传文件失败：" + response, RespErrorCode.UPLOAD_FILE_ERROR.getCode());
+            } else {
+                return new FileResp(key, getFileUrl(key));
+            }
+        } catch (Exception e) {
+            throw new FileException("上传文件失败：" + e.getMessage(), RespErrorCode.UPLOAD_FILE_ERROR.getCode());
+        }
+    }
+
+    @Override
+    public String getFileUrl(String key) {
+        return qiniuYunService.getObjectUrl(key, null);
+    }
+}

bc7446f68e2b5ea4ed956d21f0282e3cf26085c9
dayou <853094838@qq.com>
2025-06-16 07:15:32 +0800

    fix: 修复sever check

--- survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
+++ survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
@@ -1,13 +1,16 @@
 package com.xiaojusurvey.engine.common.enums;
 
 public enum SecurityScenarioEnum {
-
     DATA_ANALYSIS("数据分析"),
-    DATA_WAREHOUSING("数据入库"),
-    ;
-    final String description;
+    DATA_WAREHOUSING("数据入库");
+
+    private final String description;
 
     SecurityScenarioEnum(String description) {
         this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }

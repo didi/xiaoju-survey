2d06748242f0ff30e3df15448ef2431d187c65ca
liweitang <levitang@126.com>
2025-06-30 14:18:48 +0800

    [Feature]:安全服务，根据不同场景对数据进行加密、解密、脱敏：修复使其符合代码规范 (#491)

--- survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
+++ survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
@@ -4,14 +4,9 @@ public enum SecurityScenarioEnum {
 
     DATA_ANALYSIS("数据分析"),
     DATA_WAREHOUSING("数据入库");
-
-    private final String description;
+    final String description;
 
     SecurityScenarioEnum(String description) {
         this.description = description;
-    }
-
-    public String getDescription() {
-        return description;
     }
 }

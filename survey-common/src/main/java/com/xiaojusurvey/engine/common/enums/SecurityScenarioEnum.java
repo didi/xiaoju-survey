277b280eb3b69b4a7c36e4c857cc89647fce8e2d
liweitang <levitang@126.com>
2025-06-30 14:20:06 +0800

    Revert "[Feature]:安全服务，根据不同场景对数据进行加密、解密、脱敏：修复使其符合代码规范 (#491)"
    This reverts commit c2b09ddf6e9d148046d6040331005bbd9e7adfc7.

--- survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
+++ survey-common/src/main/java/com/xiaojusurvey/engine/common/enums/SecurityScenarioEnum.java
@@ -3,7 +3,8 @@ package com.xiaojusurvey.engine.common.enums;
 public enum SecurityScenarioEnum {
 
     DATA_ANALYSIS("数据分析"),
-    DATA_WAREHOUSING("数据入库");
+    DATA_WAREHOUSING("数据入库"),
+    ;
     final String description;
 
     SecurityScenarioEnum(String description) {
